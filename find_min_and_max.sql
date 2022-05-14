16b716fed17fb05cf883785f7100bcf06f680304
UmutTEKİN <umuttechin@gmail.com>
2022-05-14 13:54:14 +0200

    Update find_min_and_max.sql

--- find_min_and_max.sql
+++ find_min_and_max.sql
@@ -1,3 +1,10 @@
+DROP AGGREGATE IF EXISTS find_min_and_max(integer);
+DROP AGGREGATE IF EXISTS find_min_and_max(varchar);
+DROP FUNCTION IF EXISTS find_min_and_max_func(state point, next integer);
+DROP FUNCTION IF EXISTS find_min_and_max_final_func(state point);
+DROP FUNCTION IF EXISTS find_min_and_max_funcs(state point, next varchar);
+DROP FUNCTION IF EXISTS find_min_and_max_final_funcs(state point);
+
 CREATE or replace FUNCTION find_min_and_max_func(
     state point,
     next  integer
@@ -6,16 +13,16 @@ LANGUAGE plpgsql
 STRICT
 AS $$
 declare
-min_val integer;  
-max_val integer;                   
-begin    
-if state[0] <= next then min_val := state[0];  
-elsif next < state[0] then min_val := next;  
+min_val integer;
+max_val integer;
+begin
+if state[0] <= next then min_val := state[0];
+elsif next < state[0] then min_val := next;
 end if;
-if state[1] >= next then max_val := state[1];  
-elsif next > state[1] then max_val := next;  
-end if;               
-return point(min_val, max_val) ;                     
+if state[1] >= next then max_val := state[1];
+elsif next > state[1] then max_val := next;
+end if;
+return point(min_val, max_val) ;
 END;
 $$;
 
@@ -24,20 +31,21 @@ CREATE or replace FUNCTION find_min_and_max_final_func(
 ) RETURNS varchar
 LANGUAGE plpgsql
 STRICT
-AS $$                 
-begin              
-return cast(state[0] as varchar) || '->' || cast(state[1] as varchar) ;                     
+AS $$
+begin
+return cast(state[0] as varchar) || '->' || cast(state[1] as varchar) ;
 END;
 $$;
 
---In this example I am using integer. That is why 
+--In this example I am using integer. That is why
 --the boundaries of integer data type are selected
---as my initial condition states.CREATE or replace AGGREGATE find_min_and_max(integer)
+--as my initial condition states.
+CREATE or replace AGGREGATE find_min_and_max(integer)
 (
     SFUNC    = find_min_and_max_func, -- State function
     STYPE    = point,       -- State type
-	FINALFUNC = find_min_and_max_final_func,
-	initcond = '(2147483647,-2147483648)'
+        FINALFUNC = find_min_and_max_final_func,
+        initcond = '(2147483647,-2147483648)'
 );
 
 CREATE or replace FUNCTION find_min_and_max_funcs(
@@ -48,16 +56,16 @@ LANGUAGE plpgsql
 STRICT
 AS $$
 declare
-min_val integer;  
-max_val integer;                   
-begin    
-if cast(state[0] as int) <= cast(next as int) then min_val := cast(state[0] as int);  
-elsif cast(next as int) < cast(state[0] as int) then min_val := cast(next as int);  
+min_val integer;
+max_val integer;
+begin
+if cast(state[0] as int) <= cast(next as int) then min_val := cast(state[0] as int);
+elsif cast(next as int) < cast(state[0] as int) then min_val := cast(next as int);
 end if;
-if cast(state[1] as int) >= cast(next as int) then max_val := cast(state[1] as int);  
-elsif cast(next as int) > cast(state[1] as int) then max_val := cast(next as int);  
-end if;               
-return point(min_val, max_val) ;                     
+if cast(state[1] as int) >= cast(next as int) then max_val := cast(state[1] as int);
+elsif cast(next as int) > cast(state[1] as int) then max_val := cast(next as int);
+end if;
+return point(min_val, max_val) ;
 END;
 $$;
 
@@ -66,9 +74,9 @@ CREATE or replace FUNCTION find_min_and_max_final_funcs(
 ) RETURNS varchar
 LANGUAGE plpgsql
 STRICT
-AS $$                 
-begin              
-return cast(state[0] as varchar) || '->' || cast(state[1] as varchar) ;                     
+AS $$
+begin
+return cast(state[0] as varchar) || '->' || cast(state[1] as varchar) ;
 END;
 $$;
 
@@ -77,8 +85,8 @@ CREATE or replace AGGREGATE find_min_and_max(varchar)
 (
     SFUNC    = find_min_and_max_funcs, -- State function
     STYPE    = point,       -- State type
-	FINALFUNC = find_min_and_max_final_funcs,
-	initcond = '(2147483647,-2147483648)'
+        FINALFUNC = find_min_and_max_final_funcs,
+        initcond = '(2147483647,-2147483648)'
 );
 
 --For integer data type
